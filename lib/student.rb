296abb0200d57214da0430d221a55ffd9046af27
Laura Messa Travin <laura.travin@gmail.com>
2018-10-19 21:46:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -105,10 +105,9 @@ class Student
       LIMIT 1
     SQL
 
-    # DB[:conn].execute(sql,10).map do |row|
-    #     self.new_from_db(row)
-    # end.first  #to get the first element
-    
+    DB[:conn].execute(sql,10).map do |row|
+        self.new_from_db(row)
+    end.first 
   end
 
 
