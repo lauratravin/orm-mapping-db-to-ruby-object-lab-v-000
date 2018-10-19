8af8b9c299177296711f0a1b45be63fbeff8ea49
Laura Messa Travin <laura.travin@gmail.com>
2018-10-19 20:20:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,9 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_student = self.new
+    new_student.name = 
+    
   end
 
   def self.all
