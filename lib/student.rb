3e5172773e53f725e3c91a9383ca8fd268806db1
Laura Messa Travin <laura.travin@gmail.com>
2018-10-19 21:47:36 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,11 +101,11 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 10
       LIMIT ?
     SQL
 
-    DB[:conn].execute(sql,?,?).map do |row|
+    DB[:conn].execute(sql,?).map do |row|
         self.new_from_db(row)
     end.
   end
